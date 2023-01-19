176ffe7a779eb388096d2ae6e44c146e521bff76
evaits <musicevait@gmail.com>
2023-01-19 17:43:19 +0100

    fix js, css

--- script/main.js
+++ script/main.js
@@ -10,24 +10,7 @@ function close_modal() {
     inp.value = ''
 }
 
-// Add num
-function add_10() {
-    inp.value = Number(inp.value) + 10
-}
-function add_20() {
-    inp.value = Number(inp.value) + 20
-}
-function add_30() {
-    inp.value = Number(inp.value) + 30
-}
-
-// Dell num
-function dell_10() {
-    inp.value = Number(inp.value) - 10
-}
-function dell_20() {
-    inp.value = Number(inp.value) - 20
-}
-function dell_30() {
-    inp.value = Number(inp.value) - 30
+// Edid num
+function add(amount) {
+    inp.value = Number(inp.value) + amount
 }
