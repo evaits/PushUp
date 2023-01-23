45c3bfe7ce747def15d9753ba22f08f9bc46f691
evaits <musicevait@gmail.com>
2023-01-23 17:37:08 +0100

    fix edit js

--- script/edit.js
+++ script/edit.js
@@ -1,37 +1,50 @@
 if (localStorage.getItem('arr')) {
     let ul = document.querySelector('ul')
-    let li
-    
-    li = document.createElement('li')
-    li.className = "table"
+    let arr = localStorage.getItem('arr')
+    let today = localStorage.getItem('today')
 
+
+    let sum;
+    arr = arr.split(',')  
+    for(let i = 0; i<arr.length; i++){
+        arr[i] = Number(arr[i])
+    }
+    arr.map((item) => sum += item);
+
+    let li;
     let div;
-
-    div = document.createElement('div')
-    div.className = "info";
-    
-
     let span1;
     let span2;
-    span1 = document.createElement('span')
-    span1.className = "data"
-    span1.innerHTML = "22.01.23"
-
-    span2 = document.createElement('span')
-    span2.className = "number"
-    span2.innerHTML = "43"
-
-
     let btn;
 
-    btn = document.createElement('button')
-    btn.className = "btn"
-    btn.innerHTML = "Edit"
+    for(let i = 0; i<localStorage.length; i++){
+        if((localStorage.key(i)=='arr') || (localStorage.key(i) == "today")){
+            continue
+        }
+        li = document.createElement('li')
+        div = document.createElement('div')
+        span1 = document.createElement('span')
+        span2 = document.createElement('span')
+        btn = document.createElement('button')
+        
     
-    div.append(span1)
-    div.append(span2)
-    li.append(div)
-    li.append(btn)
-    ul.append(li)
+        li.className = "table"
+        div.className = "info";
+        span1.className = "data"
+        span2.className = "number"
+        btn.className = "btn"
+        
+        local = localStorage.key(i)
+        
+        span1.innerHTML = local
+        span2.innerHTML = localStorage.getItem(local)
+        btn.innerHTML = "Edit"
+        
 
+        div.append(span1)
+        div.append(span2)
+        li.append(div)
+        li.append(btn)
+        ul.prepend(li)
     }
+}
